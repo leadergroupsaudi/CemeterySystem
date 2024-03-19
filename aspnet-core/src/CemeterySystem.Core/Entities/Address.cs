e7d64cc9a41c779b3f9c1fd4355a2e2c52ad6145
mohamedsaadeldin <mohamedsaadeldin@outlook.com>
2024-03-19 15:06:05 +0200

    AddEntities

---
+++ aspnet-core/src/CemeterySystem.Core/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Domain.Entities;
+using System;
+
+namespace CemeterySystem.Entities
+{
+    public class Address : Entity<Guid>, ISoftDelete
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
