3d6c6c63dd2bef10a153c62d5fc79adc77c2b60f
mohamedsaadeldin <mohamedsaadeldin@outlook.com>
2024-03-22 01:13:23 +0200

    LookUp Designe

--- angular/src/app/volunteer/volunteer.component.ts
+++ angular/src/app/volunteer/volunteer.component.ts
@@ -1,35 +1,78 @@
+// import { Component, Injector, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { AppComponentBase } from '@shared/app-component-base';
+// import { LookUpsServiceProxy, RegionDto, VolunteerServiceProxy } from '@shared/service-proxies/service-proxies';
+
+// @Component({
+//   selector: 'app-volunteer',
+//   templateUrl: './volunteer.component.html',
+//   styleUrl: './volunteer.component.css'
+// })
+// export class VolunteerComponent extends AppComponentBase implements OnInit {
+//   regions: RegionDto[] = [];
+//   constructor(injector: Injector,
+//     private VolunteerService: VolunteerServiceProxy,
+//     private LookUpService: LookUpsServiceProxy) {
+//     super(injector);
+//   }
+  
+//   ngOnInit(): void {
+//     this.LookUpService.getRegions().subscribe((result: RegionDto[]) => {
+//       this.regions = result;
+//     })
+//   }
+// }
+
 import { Component, Injector, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AppComponentBase } from '@shared/app-component-base';
-import { LookUpsServiceProxy, RegionDto, VolunteerServiceProxy } from '@shared/service-proxies/service-proxies';
+import { CityDto, LookUpsServiceProxy, RegionDto, VolunteerServiceProxy,DistrictDto } from '@shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-volunteer',
   templateUrl: './volunteer.component.html',
-  styleUrl: './volunteer.component.css'
+  styleUrls: ['./volunteer.component.css']
 })
 export class VolunteerComponent extends AppComponentBase implements OnInit {
   regions: RegionDto[] = [];
-  constructor(injector: Injector,
-    private VolunteerService: VolunteerServiceProxy,
-    private LookUpService: LookUpsServiceProxy) {
+  cities: CityDto[] = [];
+  districts: DistrictDto[]=[];
+  selectedRegion: number;
+  selectedCity: number;
+  selectedDistrict: number;
+
+  constructor(
+    injector: Injector,
+    private volunteerService: VolunteerServiceProxy,
+    private lookUpService: LookUpsServiceProxy
+  ) {
     super(injector);
   }
 
   ngOnInit(): void {
-    this.LookUpService.getRegions().subscribe((result: RegionDto[]) => {
+    this.lookUpService.getRegions().subscribe((result: RegionDto[]) => {
       this.regions = result;
-    })
+      if (this.regions.length > 0) {
+        this.selectedRegion = this.regions[0].id;
+        this.loadCities(this.selectedRegion);
+      }
+    });
   }
-  public areaList: Array<string> = [
-    "Boston",
-    "Chicago",
-    "Houston",
-    "Los Angeles",
-    "Miami",
-    "New York",
-    "Philadelphia",
-    "San Francisco",
-    "Seattle",
-  ];
+
+  onRegionChange(): void {
+    this.loadCities(this.selectedRegion);
+  }
+
+  loadCities(regionId: number): void {
+    this.lookUpService.getCities(regionId).subscribe((result: CityDto[]) => {
+      this.cities = result;
+      // Clear districts when cities change
+      this.districts = [];
+    });
+  }
+  loadDistrictsFromCity(cityId: number): void {
+    this.lookUpService.getDistricts(cityId).subscribe((result: DistrictDto[]) => {
+      this.districts = result;
+    });
+  }
 }
+
