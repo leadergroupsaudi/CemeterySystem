6622b007660357b750db92b60a45ffe1f661d65f
Mohamed Ezz Elarab <163855168+Mohamed-Ezz-Elarab@users.noreply.github.com>
2024-03-26 14:27:57 +0200

    Add arabic localization

--- aspnet-core/src/CemeterySystem.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultSettingsCreator.cs
+++ aspnet-core/src/CemeterySystem.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultSettingsCreator.cs
@@ -30,7 +30,7 @@ namespace CemeterySystem.EntityFrameworkCore.Seed.Host
             AddSettingIfNotExists(EmailSettingNames.DefaultFromDisplayName, "mydomain.com mailer", tenantId);
 
             // Languages
-            AddSettingIfNotExists(LocalizationSettingNames.DefaultLanguage, "en", tenantId);
+            AddSettingIfNotExists(LocalizationSettingNames.DefaultLanguage, "ar", tenantId);
         }
 
         private void AddSettingIfNotExists(string name, string value, int? tenantId = null)
